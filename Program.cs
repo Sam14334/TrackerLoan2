7a2f68af564de488a6acd8b019b33008d612d4f1
Alexander Bandada <85205695+Eirosan29@users.noreply.github.com>
2026-02-21 12:31:27 +0800

    Made the initial Program with little use of functions

--- Program.cs
+++ Program.cs
@@ -4,7 +4,53 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string exampleReference = "abcd";
+            int dayPassed = 31;
+            int dueDate = 30;
+
+            int noOfLoans = 1;
+            int amount = 1000;
+            int duration = 60;
+            double interestRate = 10;
+            double penaltyValue = amount + (amount * interestRate);
+
+
+            string referenceInput = getReferenceInput();
+             
+            if (referenceInput == exampleReference)
+            {
+                displayLoanInfo(noOfLoans, amount, duration, interestRate, penaltyValue); 
+            }
+            else
+            {
+                Console.WriteLine("Invalid reference. Please try again.");
+            }
+
+            if (dayPassed > dueDate)
+            {
+                Console.WriteLine($"Loan is overdue by {dayPassed - dueDate} day/s. Penalty applied: {penaltyValue}");
+            }
+            else if (dayPassed == (dueDate - 5))
+            {
+                Console.WriteLine("Your loan is almost due, please settle it immediately.");
+            }
+        }
+
+        static string getReferenceInput()
+        {
+            Console.Write("Enter a reference id:");
+              string referenceInput = Console.ReadLine();
+            return referenceInput;
+        }
+
+        static void displayLoanInfo(int noOfLoans, int amount, int duration, double interestRate, double penaltyValue)
+        {
+            Console.WriteLine($"No of loans: {noOfLoans}");
+            Console.WriteLine($"Amount: {amount} days");
+            Console.WriteLine($"Duration: {duration}");
+            Console.WriteLine($"Interest Rate: {interestRate}%");
+            Console.WriteLine($"Due Date Penalty: {penaltyValue} Php");
         }
     }
 }
+    
