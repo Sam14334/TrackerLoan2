9f814a9cfea23bc7c8bdf61d15247cd3d2925a39
Alexander Bandada <85205695+Eirosan29@users.noreply.github.com>
2026-02-21 11:58:14 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TrackerLoan2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
